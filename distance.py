75de8d4903203ee7ed238a00faea8b761f026e97
Gamer.bili <bili33@87ouo.top>
2019-01-29 15:37:01 +0800

    .

--- distance.py
+++ distance.py
@@ -1,52 +1,14 @@
-#导入 GPIO库
-import RPi.GPIO as GPIO
+import wiringpi
 import time
- 
-#设置 GPIO 模式为 BCM
-GPIO.setmode(GPIO.BCM)
- 
-#定义 GPIO 引脚
-GPIO_TRIGGER = 28
-GPIO_ECHO = 29
- 
-#设置 GPIO 的工作方式 (IN / OUT)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
- 
-def distance():
-    # 发送高电平信号到 Trig 引脚
-    GPIO.output(GPIO_TRIGGER, True)
- 
-    # 持续 10 us 
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
- 
-    start_time = time.time()
-    stop_time = time.time()
- 
-    # 记录发送超声波的时刻1
-    while GPIO.input(GPIO_ECHO) == 0:
-        start_time = time.time()
- 
-    # 记录接收到返回超声波的时刻2
-    while GPIO.input(GPIO_ECHO) == 1:
-        stop_time = time.time()
- 
-    # 计算超声波的往返时间 = 时刻2 - 时刻1
-    time_elapsed = stop_time - start_time
-    # 声波的速度为 343m/s， 转化为 34300cm/s。
-    distance = (time_elapsed * 34300) / 2
- 
-    return distance
- 
-if __name__ == '__main__':
-    try:
-        while True:
-            dist = distance()
-            print("Measured Distance = {:.2f} cm".format(dist))
-            time.sleep(1)
- 
-        # Reset by pressing CTRL + C
-    except KeyboardInterrupt:
-        print("Measurement stopped by User")
-        GPIO.cleanup()
+
+def dis():
+    wiringpi.wiringPiSetup()
+    wiringpi.digitalWrite(28,0)
+    t1=time.time()
+    time.sleep(1)
+    wiringpi.digitalWrite(28,1)
+    t2=time.time()
+    s=(t2-t1)*340/2
+    print (s)
+while 1:
+    dis()
