1ac243bdefc66c16fb6dd95e4a44fe1d3bcd77ef
Gamer.bili <bili33@87ouo.top>
2019-01-29 16:55:55 +0800

    Completed The Project

--- distance.py
+++ distance.py
@@ -1,14 +1,26 @@
 import wiringpi
 import time
-
 def dis():
-    wiringpi.wiringPiSetup()
     wiringpi.digitalWrite(28,0)
-    t1=time.time()
-    time.sleep(1)
+    t1=time.time()  
+    time.sleep(0.00015)
     wiringpi.digitalWrite(28,1)
+    while not wiringpi.digitalRead(29):
+        t_n=time.time()
+        if (t_n - t1)>=0.1:
+            break
+    while wiringpi.digitalRead(29):
+        t_n=time.time()
+        if (t_n - t1)>=0.1:
+            break
     t2=time.time()
-    s=(t2-t1)*340/2
+    s=(t2-t1)*34000/2
     print (s)
-while 1:
-    dis()
+    print(" cm")
+    return s
+wiringpi.wiringPiSetup()
+wiringpi.pinMode(28,1)
+wiringpi.pinMode(29,0)
+#while 1:
+#    dis()
+#    time.sleep(0.1)
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import distance
 import time
-import ../acc/acc as mv
+import acc as mv
 s=0.0
 while 1:
-    distance()
+    s=distance.dis()
     if s<=80 and s>50:
         mv.forward(50)
     elif s<=50 and s>30:
         mv.forward(30)
     elif s<=30:
         mv.forward(0)
-    elif s>80
+        break
+    elif s>80:
         mv.forward(100)
     time.sleep(0.2)
